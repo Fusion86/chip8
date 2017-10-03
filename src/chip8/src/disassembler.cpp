54b81089a14a72191f1363a6c121e1d9edef2fb0
Fusion86 <fusion86@cerbus.nl>
2017-10-03 23:46:30 +0200

    Update

--- src/chip8/src/disassembler.cpp
+++ src/chip8/src/disassembler.cpp
@@ -2,7 +2,7 @@
 
 #include <string.h>
 
-#define COMMENT_PADDING_WIDTH 30
+#define COMMENT_PADDING_WIDTH 24
 
 namespace CHIP8
 {
@@ -19,7 +19,7 @@ namespace CHIP8
                 OpcodeToAsmString(opcode, asm_str, sizeof(asm_str));
                 padding = COMMENT_PADDING_WIDTH - (int)strlen(asm_str);
                 if (padding < 0) padding = 0;
-                fprintf(output, "%s %*s; %04X\n", asm_str, padding, "", opcode);
+                fprintf(output, "%s %*s; %X    op: %04X\n", asm_str, padding, "", i, opcode);
             }
         }
 
@@ -35,9 +35,10 @@ namespace CHIP8
                     snprintf(buffer, buffer_size, "RET");
                     break;
                 default:
-                    snprintf(buffer, buffer_size, "SYS addr");
+                    snprintf(buffer, buffer_size, "SYS %X", NNN);
                     break;
                 }
+                break;
             case 0x1000:
                 snprintf(buffer, buffer_size, "JP %X", NNN);
                 break;
@@ -45,21 +46,57 @@ namespace CHIP8
                 snprintf(buffer, buffer_size, "CALL %X", NNN);
                 break;
             case 0x3000:
-                snprintf(buffer, buffer_size, "SE V%X, %X", X, KK);
+                snprintf(buffer, buffer_size, "SE v%X, %X", X, KK);
                 break;
             case 0x4000:
-                snprintf(buffer, buffer_size, "SNE V%X, %X", X, KK);
+                snprintf(buffer, buffer_size, "SNE v%X, %X", X, KK);
                 break;
             case 0x5000:
-                snprintf(buffer, buffer_size, "SE V%X, V%X", X, Y);
+                snprintf(buffer, buffer_size, "SE v%X, v%X", X, Y);
                 break;
             case 0x6000:
-                snprintf(buffer, buffer_size, "LD V%X, %X", X, KK);
+                snprintf(buffer, buffer_size, "LD v%X, %i", X, KK);
                 break;
             case 0x7000:
-                snprintf(buffer, buffer_size, "ADD V%X, %X", X, KK);
+                snprintf(buffer, buffer_size, "ADD v%X, %X", X, KK);
+                break;
+            case 0x8000:
+                switch (N) {
+                case 0x0:
+                    snprintf(buffer, buffer_size, "LD v%X, v%X, %X", X, Y, KK);
+                    break;
+                }
+                break;
+            case 0xA000:
+                snprintf(buffer, buffer_size, "LD I, %X", NNN);
+                break;
+            case 0xC000:
+                snprintf(buffer, buffer_size, "RND v%X, %X", X, KK);
+                break;
+            case 0xD000:
+                snprintf(buffer, buffer_size, "DRW v%X, v%X, %i", X, Y, N);
+                break;
+            case 0xE000:
+                switch (KK) {
+                case 0xA1:
+                    snprintf(buffer, buffer_size, "SKNP v%X", X);
+                    break;
+                default: goto unknown_opcode; break;
+                }
+                break;
+            case 0xF000:
+                switch (KK) {
+                case 0x07:
+                    snprintf(buffer, buffer_size, "LD v%X, DT", X);
+                    break;
+                case 0x15:
+                    snprintf(buffer, buffer_size, "LD DT, v%X", X);
+                    break;
+                default: goto unknown_opcode; break;
+                }
                 break;
             default:
+            unknown_opcode:
                 snprintf(buffer, buffer_size, "UNKNOWN");
                 break;
             }
