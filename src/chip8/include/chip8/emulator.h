afad9848222c774cc15a3a2bcfcfca36fb112702
Fusion86 <fusion86@cerbus.nl>
2017-10-22 23:04:41 +0200

    Format update

--- src/chip8/include/chip8/emulator.h
+++ src/chip8/include/chip8/emulator.h
@@ -4,35 +4,35 @@
 
 namespace CHIP8
 {
-namespace Emulator
-{
-    class CHIP8Emulator
+    namespace Emulator
     {
-      public:
-        bool IsInitialized;
+        class CHIP8Emulator
+        {
+          public:
+            bool IsInitialized;
 
-        uint16_t Opcode;
-        uint8_t Memory[4096];
-        uint8_t V[16];
+            uint16_t Opcode;
+            uint8_t Memory[4096];
+            uint8_t V[16];
 
-        uint8_t I;
-        uint8_t Pc;
+            uint8_t I;
+            uint8_t Pc;
 
-        uint16_t Stack[16];
-        uint16_t Sp;
+            uint16_t Stack[16];
+            uint16_t Sp;
 
-        uint8_t Display[64 * 32];
+            uint8_t Display[64 * 32];
 
-        uint8_t DelayTimer;
-        uint8_t SoundTimer;
+            uint8_t DelayTimer;
+            uint8_t SoundTimer;
 
-        // uint8_t Key[16];
+            // uint8_t Key[16];
 
-        CHIP8Emulator();
+            CHIP8Emulator();
 
-        int Initialize();
-        int LoadGame();
-        int EmulateCycle();
-    };
-}
+            int Initialize();
+            int LoadGame();
+            int EmulateCycle();
+        };
+    }
 }
